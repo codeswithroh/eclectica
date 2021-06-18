5dee44d7579e3aaa0156a404c3b06a308dc72cf3
codeswithroh <codeswithrohl@example.com>
2021-06-18 10:57:09 +0530

    Added button

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,29 @@
-import './style/MyStyle.css';
-import React, {useState,useEffect} from 'react';
-import HomeVid from './assets/home.gif';
-import HomeVid2 from './assets/home2.gif';
-import { noScrollbarsClassName } from 'react-remove-scroll-bar';
+import "./style/MyStyle.css";
+import React, { useState, useEffect } from "react";
+import HomeVid from "./assets/home.gif";
+import HomeVid2 from "./assets/home2.gif";
+import { noScrollbarsClassName } from "react-remove-scroll-bar";
 const Home = () => {
-    const [isDisplay,setIsDisplay] = useState(true);
-    useEffect(() => {    
-        setTimeout(() => {
-            setIsDisplay(false);
-        }, 7000);
-    },[])
-        return(
-        <>
-        <div className={noScrollbarsClassName}>
-            <div className={isDisplay?'show':'hide'}>
-                <img src={HomeVid} className={'homeimage'}></img>
-            </div>
-            
-            <div className={isDisplay?'hide':'show'}>
-                <img src={HomeVid2} className={'homeimage'}></img>
-                
-            </div>
-             
-        </div>
-        </>
-        )
+  const [isDisplay, setIsDisplay] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsDisplay(false);
+    }, 7000);
+  }, []);
+  return (
+    <div className={noScrollbarsClassName}>
+      <div className={isDisplay ? "show" : "hide"}>
+        <img src={HomeVid} className={"homeimage"}></img>
+      </div>
 
-}
+      <div className={isDisplay ? "hide" : "show"}>
+        <img src={HomeVid2} className={"homeimage"}></img>
+      </div>
+      <div className={isDisplay ?'homepage-button-appear':'homepage-button' }>
+        <h1>Dig In</h1>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
