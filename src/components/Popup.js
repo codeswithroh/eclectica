ceca2215f439e96bf5022c044d0fb1e067eee89f
codeswithroh <codeswithrohl@example.com>
2021-06-24 20:09:38 +0530

    Added popups

---
+++ src/components/Popup.js
@@ -0,0 +1,205 @@
+import { useState } from "react";
+import "./Popup.css";
+export function Pop1() {
+  return (
+    <div className='blur'>
+      <div className='popup'>
+        <div className='popup_content'>
+          <div>
+            <img src='https://picsum.photos/200' alt='image'></img>
+          </div>
+          <div>
+            <h2>Teachers</h2>
+            <p>
+              these are the teachers who have guided us into becoming a
+              successful club
+            </p>
+            <button>Click Here to view</button>
+          </div>
+        </div>
+        <div className='popup_content'>
+          <div>
+            <img src='https://picsum.photos/200' alt='image'></img>
+          </div>
+          <div>
+            <h2>Students</h2>
+            <p>
+              these are the students whose hardwork has provided us with immense
+              oppurtiunities
+            </p>
+            <button>Click Here to view</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function Pop2() {
+  return (
+    <div className='blur'>
+      <div className='popup'>
+        <div className='popup-row'>
+          <div className='popup-col'>
+            <div>
+              <img src='https://picsum.photos/100' alt='image'></img>
+            </div>
+            <div>
+              <h2>Club1</h2>
+              <button>Click Here to view</button>
+            </div>
+          </div>
+          <div className='popup-col'>
+            <div>
+              <img src='https://picsum.photos/100' alt='image'></img>
+            </div>
+            <div>
+              <h2>Club2</h2>
+              <button>Click Here to view</button>
+            </div>
+          </div>
+        </div>
+        <div className='popup-row'>
+          <div className='popup-col'>
+            <div>
+              <img src='https://picsum.photos/100' alt='image'></img>
+            </div>
+            <div>
+              <h2>Club3</h2>
+              <button>Click Here to view</button>
+            </div>
+          </div>
+          <div className='popup-col'>
+            <div>
+              <img src='https://picsum.photos/100' alt='image'></img>
+            </div>
+            <div>
+              <h2>Club4</h2>
+              <button>Click Here to view</button>
+            </div>
+          </div>
+        </div>
+        <div className='popup-row'>
+          <div className='popup-col'>
+            <div>
+              <img src='https://picsum.photos/100' alt='image'></img>
+            </div>
+            <div>
+              <h2>Club5</h2>
+              <button>Click Here to view</button>
+            </div>
+          </div>
+          <div className='popup-col'>
+            <div>
+              <img src='https://picsum.photos/100' alt='image'></img>
+            </div>
+            <div>
+              <h2>Club6</h2>
+              <button>Click Here to view</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function Pop3() {
+  return (
+    <div className='blur'>
+      <div className='popup'>
+        <div className='popup-row'>
+          <div className='popup-col'>
+            <div>
+              <img src='https://picsum.photos/80' alt='image'></img>
+            </div>
+            <div>
+              <h2>Event1</h2>
+              <button>Click Here to view</button>
+            </div>
+          </div>
+          <div className='popup-col'>
+            <div>
+              <img src='https://picsum.photos/80' alt='image'></img>
+            </div>
+            <div>
+              <h2>Event2</h2>
+              <button>Click Here to view</button>
+            </div>
+          </div>
+          <div className='popup-col'>
+            <div>
+              <img src='https://picsum.photos/80' alt='image'></img>
+            </div>
+            <div>
+              <h2>Event3</h2>
+              <button>Click Here to view</button>
+            </div>
+          </div>
+        </div>
+        <div className='popup-row'>
+          <div className='popup-col'>
+            <div>
+              <img src='https://picsum.photos/80' alt='image'></img>
+            </div>
+            <div>
+              <h2>Event4</h2>
+              <button>Click Here to view</button>
+            </div>
+          </div>
+        </div>
+        <div className='popup-row'>
+          <div className='popup-col'>
+            <div>
+              <img src='https://picsum.photos/80' alt='image'></img>
+            </div>
+            <div>
+              <h2>Event5</h2>
+              <button>Click Here to view</button>
+            </div>
+          </div>
+          <div className='popup-col'>
+            <div>
+              <img src='https://picsum.photos/80' alt='image'></img>
+            </div>
+            <div>
+              <h2>Event6</h2>
+              <button>Click Here to view</button>
+            </div>
+          </div>
+          <div className='popup-col'>
+            <div>
+              <img src='https://picsum.photos/80' alt='image'></img>
+            </div>
+            <div>
+              <h2>Event7</h2>
+              <button>Click Here to view</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function Pop4() {
+  return (
+    <div className='blur'>
+      <div className='popup'>
+        <div className='popup-row'>
+          <div>
+            <i className='fa fa-envelope fa-3x'></i>
+          </div>
+          <div className="mail">
+            <a href='mailto:yourmail@gmail.com'><h1>yourmail@gmail.com</h1></a>
+          </div>
+        </div>
+        <div className='popup-row'>
+          <div className='social'></div>
+          <div className='social'></div>
+          <div className='social'></div>
+        </div>
+      </div>
+    </div>
+  );
+}
